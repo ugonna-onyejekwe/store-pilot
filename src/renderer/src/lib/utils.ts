54745f60548e010b02a665e31c2a664616d002dc
ugonna-onyejekwe <ugonnainnocent063@gmail.com>
2025-03-01 11:25:54 +0100

    Removed tailwind config

--- src/renderer/src/lib/utils.ts
+++ src/renderer/src/lib/utils.ts
@@ -1,10 +1,4 @@
-import { clsx, type ClassValue } from 'clsx'
 import Cookies from 'js-cookie'
-import { twMerge } from 'tailwind-merge'
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
 
 export const getError = (error) => error.response.data.message
 

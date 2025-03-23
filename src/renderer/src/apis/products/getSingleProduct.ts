65c8ff839de1e11b60e7fea63953a4d2f785727e
ugonna-onyejekwe <innocentugonna063@gmail.com>
2025-03-23 04:37:04 +0100

    paused: sell goods

--- src/renderer/src/apis/products/getSingleProduct.ts
+++ src/renderer/src/apis/products/getSingleProduct.ts
@@ -14,7 +14,6 @@ export type ProductResponse = {
   categoryId: string
   subCategory: string
   model: string
-  actionType: string
   hasModel: boolean
   hasSubProducts: boolean
   hasSubCategory: boolean
--- src/renderer/src/components/ProductBox/index.tsx
+++ src/renderer/src/components/ProductBox/index.tsx
@@ -1,19 +1,16 @@
+import { ProductResponse } from '@renderer/apis/products/getSingleProduct.js'
 import { useRef, useState } from 'react'
+import SellProductModal from '../sellProductModal.tsx'
 import { Icons } from '../ui/icons'
 import './styles.scss'
 
 type ProductBoxType = {
-  data: {
-    subcategory: string
-    totalQuantity: number
-    model: string
-    colors: string[]
-    designs: string[]
-  }
+  data: ProductResponse
 }
 
 const ProductBox = ({ data }: ProductBoxType) => {
   const [showDropDown, setShowDropDown] = useState(false)
+  const [isSellingProduct, setIsSellingProduct] = useState(false)
   const dropdownCon = useRef(null)
 
   // window.addEventListener('click', (e) => {
@@ -26,49 +23,61 @@ const ProductBox = ({ data }: ProductBoxType) => {
   // })
 
   return (
-    <div className="product_box">
-      <div className="header">
-        <small>{data.subcategory}</small>
+    <>
+      <div className="product_box">
+        <div className="header">
+          <small>{data.subCategory}</small>
 
-        <div className="dropdown_con" ref={dropdownCon}>
-          <span onClick={() => setShowDropDown(true)}>
-            <Icons.DotMenu className="dot_menu" />
-          </span>
+          <div className="dropdown_con" ref={dropdownCon}>
+            <span onClick={() => setShowDropDown(true)}>
+              <Icons.DotMenu className="dot_menu" />
+            </span>
 
-          <div className={showDropDown ? 'dropDown active' : 'dropDown'}>
-            <li>View details</li>
-            <li>View left over</li>
+            <div className={showDropDown ? 'dropDown active' : 'dropDown'}>
+              <li>View details</li>
+              <li>View left over</li>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="modal_con">
-        <span>{data.totalQuantity}</span>#{data.model}
-      </div>
-
-      <div className="color_con">
-        <h3>Colours:</h3>
-
-        <div className="box_con">
-          {data.colors.map((i, key) => (
-            <span key={key}>{i}</span>
-          ))}
+        <div className="modal_con">
+          <span>{data.totalQuantity}</span>#{data.model}
         </div>
-      </div>
 
-      <div className="designs_con">
-        <h3>Designs:</h3>
+        <div className="color_con">
+          <h3>Colours:</h3>
 
-        <div className="box_con">
-          {data.designs.map((i, key) => (
-            <span key={key}>{i}</span>
-          ))}
+          <div className="box_con">
+            {data.colors.map((i, key) => (
+              <span key={key}>{i.name}</span>
+            ))}
+          </div>
         </div>
+
+        <div className="designs_con">
+          <h3>Designs:</h3>
+
+          <div className="box_con">
+            {data.designs.map((i, key) => (
+              <span key={key}>{i.colorName}</span>
+            ))}
+          </div>
+        </div>
+
+        {/* sell btn */}
+        <button className="sell_btn" onClick={() => setIsSellingProduct(true)}>
+          Sell
+        </button>
       </div>
 
-      {/* sell btn */}
-      <button className="sell_btn">Sell</button>
-    </div>
+      {isSellingProduct && (
+        <SellProductModal
+          onOpen={isSellingProduct}
+          onOpenChange={setIsSellingProduct}
+          data={data}
+        />
+      )}
+    </>
   )
 }
 
